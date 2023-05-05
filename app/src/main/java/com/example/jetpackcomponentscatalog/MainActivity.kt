c5fb6f201610935d913a96b9d1a5942b4ee2b28c
Erich Schnell <schnellerich@gmail.com>
2023-05-04 21:00:03 -0300

    TextField
    En este apartado se vio un poco de:
    - TextField
    - TextFieldAvance
    - OutlinedTextField

--- app/src/main/java/com/example/jetpackcomponentscatalog/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomponentscatalog/MainActivity.kt
@@ -5,17 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomponentscatalog.ui.theme.JetpackComponentsCatalogTheme
 
@@ -26,10 +27,11 @@ class MainActivity : ComponentActivity() {
             JetpackComponentsCatalogTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    MyText()
+                    Column() {
+                        MyOutLinedTextField()
+                    }
                 }
             }
         }
@@ -40,12 +42,47 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     JetpackComponentsCatalogTheme {
-        MyText()
+        MyOutLinedTextField()
     }
 }
 
 @Composable
-fun MyText(){
+fun MyOutLinedTextField() {
+
+    var myTextFiel by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = myTextFiel,
+        onValueChange = { myTextFiel = it },
+        modifier = Modifier.padding(24.dp),
+        label = { Text(text = "Holita") },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Red,
+            unfocusedBorderColor = Color.Blue
+        )
+    )
+}
+
+@Composable
+fun MyTextFieldAdvance() {
+    var myTextFiel by remember { mutableStateOf("") }
+    TextField(value = myTextFiel, onValueChange = {
+        myTextFiel = if (it.contains("ericherich")) {
+            it.replace("erich", "lujan")
+        } else {
+            it
+        }
+    }, label = { Text(text = "Introduce tu nombre") })
+}
+
+@Composable
+fun MyTextField() {
+
+    var myTextFiel by remember { mutableStateOf("") }
+    TextField(value = myTextFiel, onValueChange = { myTextFiel = it })
+}
+
+@Composable
+fun MyText() {
     Column(Modifier.fillMaxSize()) {
         Text(text = "Esto es un ejemplo")
         Text(text = "Esto es un ejemplo", color = Color.Red)
@@ -54,9 +91,11 @@ fun MyText(){
         Text(text = "Esto es un ejemplo", fontFamily = FontFamily.Cursive)
         Text(text = "Esto es un ejemplo", textDecoration = TextDecoration.LineThrough)
         Text(text = "Esto es un ejemplo", textDecoration = TextDecoration.Underline)
-        Text(text = "Esto es un ejemplo", textDecoration = TextDecoration.combine(
-            listOf(TextDecoration.Underline, TextDecoration.LineThrough)
-        ))
+        Text(
+            text = "Esto es un ejemplo", textDecoration = TextDecoration.combine(
+                listOf(TextDecoration.Underline, TextDecoration.LineThrough)
+            )
+        )
         Text(text = "Esto es un ejemplo", fontSize = 30.sp)
     }
 }
